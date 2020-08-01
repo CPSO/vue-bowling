f97b37fe3c569a65cbcfdcb86152e2c17eb1053c
CPSO <5735792+CPSO@users.noreply.github.com>
2020-08-01 23:11:45 +0200

    jest score logic test setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  silent: true,
+  verbose: true,
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+      "src/components/*.{js,vue}",      
+      "!**/node_modules/**"
+  ],
+  coverageReporters: [
+      "html",
+      "text-summary"
+  ]
 }
--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -1,9 +1,8 @@
 import { mount } from '@vue/test-utils'
-import Home from '@/components/home.vue'
 import { BootstrapVue} from 'bootstrap-vue'
-
-
 import { createLocalVue } from '@vue/test-utils'
+import Home from '@/components/home.vue'
+import store from '@/store/index.js'
 
 // create an extended `Vue` constructor
 const localVue = createLocalVue()
@@ -26,4 +25,71 @@ describe('Home.vue', () => {
         const wrapper = mount(Home,{localVue})
         expect(wrapper.exists()).toBe(true);
     })
+})
+
+describe('Points Algorithm test', () => {
+
+  test('Ask for points [6,1]', () => {
+    store.commit('UPDATE_BSCORES', [[6,1]])          
+    let score = store.getters.getBScore   
+    console.log(`Submitted Points: `)   
+    console.log(store.getters.getBPointsRaw) 
+    console.log(`Calculated Score: ${score}`)
+    expect(score).toEqual([7]);
+  })
+
+
+  test('Ask for points [[3,5], [1,1], [7,1], [8,1], [1,3]]', () => {
+    store.commit('UPDATE_BSCORES', [[3,5],[1,1],[7,1],[8,1],[1,3]])  
+    let score = store.getters.getBScore 
+    console.log(`Submitted Points: `)   
+    console.log(store.getters.getBPointsRaw) 
+    console.log(`Calculated Score: ${score}`)    
+    expect(score).toEqual([8,10,18,27,31]);
+  })
+
+  test('Ask for points [[3,3]', () => {
+    store.commit('UPDATE_BSCORES', [[3,3]])  
+    let score = store.getters.getBScore 
+    console.log(`Submitted Points: `)   
+    console.log(store.getters.getBPointsRaw) 
+    console.log(`Calculated Score: ${score}`)    
+    expect(score).toEqual([6]);
+  })
+  
+  test('Ask for points [[6,0],[1,2],[4,2],[4,5],[2,7],[8,1],[7,3],[8,1],[0,0],[9,0]]', () => {
+    store.commit('UPDATE_BSCORES', [[6,0],[1,2],[4,2],[4,5],[2,7],[8,1],[7,3],[8,1],[0,0],[9,0]])  
+    let score = store.getters.getBScore 
+    console.log(`Submitted Points: `)   
+    console.log(store.getters.getBPointsRaw) 
+    console.log(`Calculated Score: ${score}`)    
+    expect(score).toEqual([6,9,15,24,33,42,60,69,69,78]);
+  })
+
+  test('Ask for points [[3,1],[6,1],[2,4],[0,4],[8,0],[10,0],[4,1],[8,2],[4,6]]', () => {
+    store.commit('UPDATE_BSCORES', [[3,1],[6,1],[2,4],[0,4],[8,0],[10,0],[4,1],[8,2],[4,6]])  
+    let score = store.getters.getBScore 
+    console.log(`Submitted Points: `)   
+    console.log(store.getters.getBPointsRaw) 
+    console.log(`Calculated Score: ${score}`)    
+    expect(score).toEqual([4,11,17,21,29,44,49,63,73]);
+  })
+
+  test('Ask for points [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10]]', () => {
+    store.commit('UPDATE_BSCORES', [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10]])  
+    let score = store.getters.getBScore 
+    console.log(`Submitted Points: `)   
+    console.log(store.getters.getBPointsRaw) 
+    console.log(`Calculated Score: ${score}`)    
+    expect(score).toEqual([30,60,90,120,150,180,210,240,270,300]);
+  })
+  
+  test('Ask for points [[4,2],[6,0],[10,0],[7,0],[0,9],[7,2],[2,4],[2,3]]', () => {
+    store.commit('UPDATE_BSCORES', [[4,2],[6,0],[10,0],[7,0],[0,9],[7,2],[2,4],[2,3]])  
+    let score = store.getters.getBScore 
+    console.log(`Submitted Points: `)   
+    console.log(store.getters.getBPointsRaw) 
+    console.log(`Calculated Score: ${score}`)    
+    expect(score).toEqual([6,12,29,36,45,54,60,65]);
+  })
 })
